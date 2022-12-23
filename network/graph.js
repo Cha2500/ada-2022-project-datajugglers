4e8053896bd6862499df00041132c57643f8f1f4
AleHC <ale.hc@ciencias.unam.mx>
2022-12-23 17:05:01 +0100

    update network

--- network/graph.js
+++ network/graph.js
@@ -6,10 +6,10 @@ async function readJSON(path) {
 
 const gData = readJSON("graph1.json");
 const Graph = ForceGraph3D()(document.getElementById('3d-graph')).graphData(gData)
-	.nodeLabel("id").nodeAutoColorBy("rating").linkOpacity(0.5).nodeThreeObject(node => {
+	.nodeLabel("id"); /*.nodeAutoColorBy("rating").linkOpacity(0.5).nodeThreeObject(node => {
 		const sprite = new SpriteText(node.id + ".jpg");
 		sprite.material.depthWrite = false; // make sprite background transparent
 		sprite.color = node.color;
 		sprite.textHeight = 8;
 		return sprite;
-	});
+	});*/
