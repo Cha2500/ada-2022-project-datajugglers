1c0f6baf271442c6a556da87d96e1575799dd15d
AleHC <ale.hc@ciencias.unam.mx>
2022-12-23 17:46:44 +0100

    update network

--- network/graph.js
+++ network/graph.js
@@ -4,23 +4,19 @@ async function readJSON(path) {
 	return json;
 }
 
+const center = "Tom Cruise"
+
 readJSON("graph1.json").then(gData => {
 	const graph = ForceGraph3D()(document.getElementById('3d-graph')).graphData(gData)
 		.nodeLabel("id").nodeAutoColorBy("rating").linkOpacity(0.25).nodeThreeObject(node => {
-			return new THREE.TextureLoader().load(
-				`${node.id}.jpg`,  // file path
-				texture => {  // on success
-					const material = new THREE.SpriteMaterial({map: texture});
-					const sprite = new THREE.Sprite(material);
-					sprite.scale.set(12, 12);
-					return sprite;
-				},
-				undefined,  // on progress
-				err => {  // on error
-					return false;
-				}
-			)
+			if (node.id == center) {
+				const texture = new THREE.TextureLoader().load(`${node.id}.jpg`);
+				const material = new THREE.SpriteMaterial({map: texture});
+				const sprite = new THREE.Sprite(material);
+				sprite.scale.set(12, 12);
+				return sprite;
+			} else {
+				return false;
+			}
 		});
-
-
 });
--- temp.md
+++ temp.md
@@ -1,4 +1,4 @@
-Look, a graph v0.16:
+Look, a graph v0.17:
 
 <p align="center">
 <iframe src="network/main.html" width="800" height="300"/></iframe>
