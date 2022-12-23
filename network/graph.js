71dff510f5e9b75188974c38e359e8c2260168b2
AleHC <ale.hc@ciencias.unam.mx>
2022-12-23 17:24:36 +0100

    update network

--- network/graph.js
+++ network/graph.js
@@ -7,10 +7,10 @@ async function readJSON(path) {
 readJSON("graph1.json").then(gData => {
 	const Graph = ForceGraph3D()(document.getElementById('3d-graph')).graphData(gData)
 		.nodeLabel("id").nodeAutoColorBy("rating").linkOpacity(0.5).nodeThreeObject(node => {
-			const sprite = new SpriteText(node.id + ".jpg");
-			sprite.material.depthWrite = false; // make sprite background transparent
-			sprite.color = node.color;
-			sprite.textHeight = 8;
-			return sprite;
-		});
+		const imgTexture = new THREE.TextureLoader().load(`./imgs/${node.id}.jpg`);
+		const material = new THREE.SpriteMaterial({ map: imgTexture });
+		const sprite = new THREE.Sprite(material);
+		sprite.scale.set(12, 12);
+		return sprite;
+	});
 });
