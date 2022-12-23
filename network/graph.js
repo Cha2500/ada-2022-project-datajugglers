2cedd2fa22c9307b05fa3813e3d51a7713e0bd96
AleHC <ale.hc@ciencias.unam.mx>
2022-12-23 18:13:52 +0100

    update network

--- network/graph.js
+++ network/graph.js
@@ -6,11 +6,33 @@ async function readJSON(path) {
 
 const center = "Tom Cruise";
 
+function colorInterpolation(level) {
+	const color1 = [239, 138, 98];
+	const color2 = [247, 247, 247];
+	const color3 = [247, 247, 247];
+
+	color = [0, 0, 0]
+	for (let i = 0; i < 3; i++) {
+		if (level <= 0.5) {
+			color[i] = 2*level*color1[i] + (1 - 2*level)*color2[i];
+		} else {
+			color[i] = 2*(level - 0.5)*color2[i] + (1 - 2*(level - 0.5))*color3[i];
+		}
+	}
+	return [~~color[0], ~~color[1], ~~color[2]];
+}
+
 readJSON("graph1.json").then(gData => {
+
+	const worstColor = [255, 0, 0];
+	const worstColor = [255, 0, 0];
 	const graph = ForceGraph3D()(document.getElementById('3d-graph')).graphData(gData)
 		.nodeLabel(node => {
-			return `Actor: ${node.id}.\nQuality: ${node.rating}`;
-		}).nodeAutoColorBy("rating").linkOpacity(0.25).nodeThreeObject(node => {
+			return `Actor: ${node.id}. Quality: ${node.rating}`;
+		}).nodeColor(node => {
+			const color = colorInterpolation(node.rating/10);
+			return `rgb(${color[0]}, ${color[1]}, ${color[2]}`;
+		}).linkOpacity(0.25).nodeThreeObject(node => {
 			if (node.id == center) {
 				const texture = new THREE.TextureLoader().load(`${node.id}.jpg`);
 				const material = new THREE.SpriteMaterial({map: texture});
--- temp.md
+++ temp.md
@@ -1,4 +1,4 @@
-Look, a graph v1.2:
+Look, a graph v1.3:
 
 <p align="center">
 <iframe src="network/main.html" width="800" height="800"/></iframe>
