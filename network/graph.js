0030642755249a1df68ad142f93a3224361b2b73
AleHC <ale.hc@ciencias.unam.mx>
2022-12-23 17:32:44 +0100

    update network

--- network/graph.js
+++ network/graph.js
@@ -6,8 +6,8 @@ async function readJSON(path) {
 
 readJSON("graph1.json").then(gData => {
 	const Graph = ForceGraph3D()(document.getElementById('3d-graph')).graphData(gData)
-		.nodeLabel("id").nodeAutoColorBy("rating").linkOpacity(0.5).nodeThreeObject(node => {
-			THREE.TextureLoader().load(
+		.nodeLabel("id").nodeAutoColorBy("rating").linkOpacity(0.25).nodeThreeObject(node => {
+			(new THREE.TextureLoader()).load(
 				`${node.id}.jpg`,  // file path
 				texture => {  // on success
 					const material = new THREE.SpriteMaterial({map: texture});
--- temp.md
+++ temp.md
@@ -1,4 +1,4 @@
-Look, a graph v0.13:
+Look, a graph v0.14:
 
 <p align="center">
 <iframe src="network/main.html" width="800" height="300"/></iframe>
