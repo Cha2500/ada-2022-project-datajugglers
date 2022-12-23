3b786896341873ad70bc4670b31c1cbaf0078c81
AleHC <ale.hc@ciencias.unam.mx>
2022-12-23 16:42:23 +0100

    update network

--- network/graph.js
+++ network/graph.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
 const gData = JSON.parse(fs.readFileSync("graph1.json"));
-const Graph = ForceGraph3D()(
-	document.getElementById('3d-graph')).graphData(gData).nodeLabel("id").nodeAutoColorBy("rating").linkOpacity(0.5);
-);
+const Graph = ForceGraph3D()(document.getElementById('3d-graph')).graphData(gData)
+	.nodeLabel("id").nodeAutoColorBy("rating").linkOpacity(0.5);
--- temp.md
+++ temp.md
@@ -1,4 +1,4 @@
-Look, a graph v1.0:
+Look, a graph v0.1:
 
 <p align="center">
 <iframe src="network/main.html" width="800" height="300"/></iframe>
